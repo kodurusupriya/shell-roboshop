d9dac63c3bad20629d399c955929d56098f593b0
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-10 22:22:28 -0500

    actions

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SG_ID="sg-0222d30c73ff37273" #replace with your id
+AMI_ID="ami-0220d79f3f480ecf5" #replace with your id
+for instance in $@
+do
+    instance_id = $(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t3.micro \
+        --security-group-ids $SG_ID \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+        --query 'Instances[0].InstanceId' \
+        --output text)
+
+    if [ $instance == "frontend"]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids i-047312789ff3c4e6a \
+            --query "Reservations[*].Instances[*].PublicIpAddress" \
+            --output text
+        ) 
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids i-047312789ff3c4e6a \
+            --query "Reservations[*].Instances[*].PrivateIpAddress" \
+            --output text
+        ) 
+    fi
+done
