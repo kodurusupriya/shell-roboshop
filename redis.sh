5492fafda9e4a46c547b21653c93620974e71cd3
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-12 21:13:15 -0500

    actions

---
+++ redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+set -e # this will be checking for errors, if errors it will exit
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+ 
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+#by default shell will not execute,only executed when called
+VALIDATE(){
+
+        if [ $1 -ne 0 ]; then 
+            echo -e "$2 ... $R failure $N" | tee -a $LOGS_FILE
+            exit 1
+        else 
+            echo -e "$2 ... $G success $N"  | tee -a $LOGS_FILE
+        fi     
+
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and Started Redis"
