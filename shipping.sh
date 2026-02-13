8cdd46b6f11cc674a0281c1ab7f5757464d3e646
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-12 22:19:45 -0500

    actions

--- shipping.sh
+++ shipping.sh
@@ -66,7 +66,7 @@ VALIDATE $? "created systemctl service"
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MYSQL"
 
-mysql -h $MYSQL_HOST-uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
 
