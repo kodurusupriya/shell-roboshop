7e032213470270e84d331816bf6a1255e393322d
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-12 22:45:22 -0500

    actions

---
+++ rabbitmq.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.supriya1999.online
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N"  | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+#by default shell will not execute,only executed when called
+VALIDATE(){
+
+        if [ $1 -ne 0 ]; then 
+            echo -e "$2 ... $R failure $N" | tee -a $LOGS_FILE
+            exit 1
+        else 
+            echo -e "$2 ... $G success $N"  | tee -a $LOGS_FILE
+        fi     
+
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added rabbitmq repo"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "installing rabbitmq server"
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+VALIDATE $? "Enabled and started rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created user and given permissions"
+
