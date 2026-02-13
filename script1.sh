ff2580ec498bbe05ef418f509dbdcc23b95caf42
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-13 00:31:45 -0500

    actions

--- script1.sh
+++ script1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 COUNTRY=INDIA
 echo "i am in :$COUNTRY
-echo :PID of script-1: $$"
+echo :PID of script1: $$"
 sh script2.sh
---
+++ script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+COUNTRY=INDIA
+echo "i am in :$COUNTRY
+echo :PID of script1: $$"
