37ad53355d4c9129fc08c99ed7b15fae953995d7
kodurusupriya <supriyanaidu113@gmail.com>
2026-02-12 23:40:16 -0500

    actions

--- frontend.sh
+++ frontend.sh
@@ -31,11 +31,11 @@ VALIDATE(){
 dnf module disable nginx -y &>>$LOGS_FILE
 dnf module enable nginx:1.24 -y &>>$LOGS_FILE
 dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Installing Nginx"
+VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGS_FILE
 systemctl start nginx 
-VALIDATE $? "Enabled and Started Nginx"
+VALIDATE $? "Enabled and Started nginx"
 
 rm -rf /usr/share/nginx/html/* 
 VALIDATE $? "Remove default content"
@@ -51,6 +51,6 @@ cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "copied our nginx conf file"
 
 systemctl restart nginx
-VALIDATE $? "Restarted Nginx"
+VALIDATE $? "Restarted nginx"
 
 
